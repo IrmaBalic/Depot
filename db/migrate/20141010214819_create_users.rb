ea6c718ff777a5ef7762598bce17e38db5fe8983
Irma <irmabalic1@gmail.com>
2014-10-23 09:57:07 +0200

    Update user

--- db/migrate/20141010214819_create_users.rb
+++ db/migrate/20141010214819_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :password_digest
       t.timestamps null: false
+      t.string :email
     end
   end
 end
--- db/migrate/20141021095031_add_address_to_users.rb
+++ db/migrate/20141021095031_add_address_to_users.rb
@@ -1,5 +1,6 @@
 class AddAddressToUsers < ActiveRecord::Migration
   def change
     add_reference :users, :address, index: true
+    add_reference :users, :roles
   end
 end
