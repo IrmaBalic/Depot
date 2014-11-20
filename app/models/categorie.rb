be3f0c3d83dc0f16b5356624074f7d2fa886d2ae
Irma <irmabalic1@gmail.com>
2014-11-21 00:49:07 +0100

    Bootstrap bug

--- app/models/categorie.rb
+++
@@ -1,8 +0,0 @@
-class Categorie < ActiveRecord::Base
-		has_many :products
-		has_many :ordered_products
-		def self.dropdown_options 
-			options = Categorie.order(:name).map{|r| [r.name, r.id] }
-			[["Please select", -1]] + options 
-		end
-end
--- app/views/addresses/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @address, :id, :name
