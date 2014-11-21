364da5972b521995215074993b1295126eb66ffd
Irma <irmabalic1@gmail.com>
2014-11-21 20:25:51 +0100

    Responsive design, homepage and categories

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		$(this).css("display","none");
 		var element = $(".cart_button_buy").index(this);
 		var loading = $(".cart_button_loading").get(element);
-		$(loading).css("display","block");
+		$(loading).css("display","inline-block");
 		$(loading).addClass("click_loading");
 		var done = $(".cart_button_buy").get(element);
 		setTimeout(function(){ 
@@ -12,7 +12,26 @@ $(document).ready(function() {
 		setTimeout(function(){
 			$(loading).removeClass("click_checked");
 			$(loading).css("display","none");
-			$(done).css("display","block");
+			$(done).css("display","none");
 		}, 5000);
 	});
+	$(".entry").hover(function() {
+		var mq = window.matchMedia( "(max-width: 768px)" );
+		if(!mq.matches) {
+			var element = $(".entry").index(this);
+			var loading = $(".cart_button_loading").get(element);
+			if($(loading).css("display") == "none") {
+				var add_to_cart = $(".button_store").get(element);
+				$(add_to_cart).css("display","inline-block");
+			}
+		}
+	}, function() {
+		var mq = window.matchMedia( "(max-width: 768px)" );
+		if(!mq.matches) {
+			var element = $(".entry").index(this);
+			var add_to_cart = $(".button_store").get(element);
+			$(add_to_cart).css("display","none");
+		}
+		}
+	);
 });
