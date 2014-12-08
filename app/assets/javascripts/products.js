f8528c40ec5aad594f1509ac95d74df935f32f93
Irma <irmabalic1@gmail.com>
2014-12-08 11:56:39 +0100

    Product validation

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,23 +1,63 @@
-/*$(document).ready(function() {
-	$(".cart_button_buy").click(function() {
-		$(this).css("display","none");
-		var element = $(".cart_button_buy").index(this);
-		var loading = $(".cart_button_loading").get(element);
-		$(loading).css("display","inline-block");
-		$(loading).addClass("click_loading");
-		var done = $(".cart_button_buy").get(element);
-		setTimeout(function(){ 
-			$(loading).addClass("click_checked").removeClass("click_loading");
-		}, 2000);
-		setTimeout(function(){
-			$(loading).removeClass("click_checked");
-			$(loading).css("display","none");
-			$(".cart_button_buy").css("display","inline-block");
-		}, 5000);
-	});
-});*/
 $(function() {
 	$('.bxslider').bxSlider({
 	  pagerCustom: '#bx-pager'
+	});
+	var category = $("#category");
+	var category_sel = category.value;
+	var product_title = $("#product_title");
+	var product_price = $("#product_price");
+	var submit = $("#submit_form");
+	// Functions
+	function show_warning(field) {
+		field.css("border-color", "#dd4b39"); 
+		var warning_label = null;
+		if (locale  == "bs")
+			warning_label = $("<p></p>").text("Ovo polje ne može ostati prazno.");
+		else 
+			warning_label = $("<p></p>").text("This field can't be empty.");
+		warning_label.attr("class","warning");
+		warning_label.css("color","#dd4b39");
+		field.parent().append(warning_label);
+	}
+	function remove_warning(field) {
+		field.parent().children("p").remove(".warning");
+		field.css("border-color", "#b7b7b7");
+	}
+	function validate_field(field) {
+		var number_of_children = field.parent().children().length;
+		if (!field.val()) {
+			if(number_of_children == 3) show_warning(field);
+		}
+		else {
+			if (number_of_children > 3) remove_warning(field);
+		}
+	}
+	//Onblur events	
+	/*category.blur(function(){
+		validate_field($(this));
+	});*/
+	product_title.blur(function(){
+		validate_field($(this));
+	});
+	product_price.blur(function(){
+		validate_field($(this));
+	});
+	// Onsubmit validation
+	$("form").submit(function() {
+		var has_empty = false;
+		$("#product_title, #product_price", this).each(function() {
+			if(!$(this).val()) {
+				has_empty = true;
+				validate_field($(this));			
+			}
+		});
+		if(category_sel == -1) {
+			has_empty = true;
+		}
+		if (has_empty)  {
+			alert("Forma nije ispravno popunjena.");
+			return false;
+		}
+		return true;
 	});
 });
