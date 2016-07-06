9609853b170ef2825c9985f8aa9948401ccec9ff
Irma Balic <irmabalic1@gmail.com>
2016-07-07 01:05:54 +0200

    small change test

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -1,10 +1,10 @@
 //= require i18n
 $(document).ready(function() {
-	$("#same_as_shipping").change(function() {
+	/*$("#same_as_shipping").change(function() {
 		if($(this).is(":checked"))
 			$("#input_form_2").css("display","none");
 		else
 			$("#input_form_2").css("display","block");
-	});
+	});*/
 });
 
